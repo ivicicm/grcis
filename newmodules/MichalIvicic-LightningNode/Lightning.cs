fbfbfda0b4b3c1dbef318ee28256cc97f1f228c5
Michal Ivičič <ivicic@netglade.cz>
2020-06-06 21:00:32 +0200

    + glowing material

--- newmodules/MichalIvicic-LightningNode/Lightning.cs
+++ newmodules/MichalIvicic-LightningNode/Lightning.cs
@@ -8,13 +8,35 @@ namespace MichalIvicic
 {
   public static class Lightning
   {
-    public static ISceneNode CreateLightning(Vector3d[] points, double r)
+    public static ISceneNode CreateLightning(Vector3d[] points, double r, double innerR = 0.03)
     {
       CSGInnerNode ltn = new CSGInnerNode( SetOperation.Union );
+
+      RecursionFunction del = (Intersection intersection, Vector3d dir, double importance, out RayRecursion rr) =>
+      {
+        double direct = 1.0 - intersection.TextureCoord.X;
+        if (direct >= (1 - innerR))
+          direct = 1;
+        else
+        {
+          direct /= 1 - innerR;
+          direct *= 0.9;
+        }
+        direct = Math.Pow(direct, 8.0);
+
+        rr = new RayRecursion(
+          new double[] {direct, direct, direct},
+          new RayRecursion.RayContribution(intersection, dir, importance));
+
+        return 144L;
+      };
 
       foreach (Vector3d point in points)
       {
         SphereFront sphere = new SphereFront();
+
+        sphere.SetAttribute(PropertyName.RECURSION, del);
+        sphere.SetAttribute(PropertyName.NO_SHADOW, true);
         ltn.InsertChild(sphere, Matrix4d.Scale(r) * Matrix4d.CreateTranslation(point));
       }
 
@@ -23,17 +45,10 @@ namespace MichalIvicic
         double dist = Vector3d.Distance(points[i], points[i + 1]);
         ISolid cilinder = new CylinderFront(0, dist);
 
-        RecursionFunction del = (Intersection ints, Vector3d dir, double importance, out RayRecursion rr) =>
-        {
-          rr = new RayRecursion(
-            new double[] { 255, 0, 0 });
-
-          return 144L;
-        };
         cilinder.SetAttribute(PropertyName.RECURSION, del);
         cilinder.SetAttribute(PropertyName.NO_SHADOW, true);
 
-        ltn.InsertChild(cilinder, Matrix4d.Scale(r, r, 1) * Matrix4d.Rotate(GetRotationBetween(Vector3d.UnitZ, Vector3d.Subtract(points[i+1], points[i]))) *  Matrix4d.CreateTranslation(points[i]));
+        ltn.InsertChild(cilinder,  Matrix4d.Scale(r, r, 1) * Matrix4d.Rotate(GetRotationBetween(Vector3d.UnitZ, Vector3d.Subtract(points[i+1], points[i]))) *  Matrix4d.CreateTranslation(points[i]));
       }
 
       return ltn;
